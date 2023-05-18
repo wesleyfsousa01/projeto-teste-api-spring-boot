de79b62460523048475aad7c2025e63ee87ef5df
wesleyfsousa01 <wesleyfsouza10@gmail.com>
2023-05-18 10:44:15 -0300

    Projeto inicializado

---
+++ src/test/java/projetoteste/apispringboot/ApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package projetoteste.apispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
